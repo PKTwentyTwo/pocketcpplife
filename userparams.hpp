8ce8f7afa1c365d3d8fdabf6e46162d275951348
PK22 <PKTwentyTwo@outlook.com>
2026-08-29 20:53:16 +0100

    Remove extraneous files

--- userparams.hpp
+++
@@ -1,7 +0,0 @@
-// User defined parameters. You can change these as necessary.
-#pragma once
-// The maximum period used when detecting the period of a pattern:
-#define MAX_PERIOD 1024
-// The unordered map used for advancing patterns. Make sure to include it!
-#include <unordered_map>
-#define umap std::unordered_map
